32507ae57158efde7a013334deb80e261497b4ed
Stefan Schweter <stefan@schweter.it>
2025-10-20 19:00:30 +0200

    feat: add first try of pretraining script

---
+++ train_model.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# This script is the "Best ChatGPT clone that $100 can buy",
+# It is designed to run in ~4 hours on 8XH100 node at $3/GPU/hour.
+
+# 1) Example launch (simplest):
+# bash speedrun.sh
+# 2) Example launch in a screen session (because the run takes ~4 hours):
+# screen -L -Logfile speedrun.log -S speedrun bash speedrun.sh
+# 3) Example launch with wandb logging, but see below for setting up wandb first:
+# WANDB_RUN=speedrun screen -L -Logfile speedrun.log -S speedrun bash speedrun.sh
+
+# Default intermediate artifacts directory is in ~/.cache/nanochat
+export OMP_NUM_THREADS=1
+export NANOCHAT_BASE_DIR="$HOME/.cache/nanochat"
+mkdir -p $NANOCHAT_BASE_DIR
+
+# -----------------------------------------------------------------------------
+# Python venv setup with uv
+
+# install uv (if not already installed)
+command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
+# create a .venv local virtual environment (if it doesn't exist)
+[ -d ".venv" ] || uv venv
+# install the repo dependencies
+uv sync
+# activate venv so that `python` uses the project's venv instead of system python
+source .venv/bin/activate
+
+# -----------------------------------------------------------------------------
+# wandb setup
+# If you wish to use wandb for logging (it's nice!, recommended).
+# 1) Make sure to first log in to wandb, e.g. run:
+#    `wandb login`
+# 2) Set the WANDB_RUN environment variable when running this script, e.g.:
+#    `WANDB_RUN=d26 bash speedrun.sh`
+if [ -z "$WANDB_RUN" ]; then
+    # by default use "dummy" : it's handled as a special case, skips logging to wandb
+    WANDB_RUN=dummy
+fi
+
+# -----------------------------------------------------------------------------
+# During the course of the run, we will be writing markdown reports to the report/
+# directory in the base dir. This command clears it out and writes a header section
+# with a bunch of system info and a timestamp that marks the start of the run.
+python -m nanochat.report reset
+
+# -----------------------------------------------------------------------------
+# Download already trained tokenizer
+wget -LO $NANOCHAT_BASE_DIR/tokenizer/token_bytes.pt https://huggingface.co/stefan-it/nanochat-german-tokenizer/resolve/main/token_bytes.pt?download=true
+wget -LO $NANOCHAT_BASE_DIR/tokenizer/tokenizer.pkl https://huggingface.co/stefan-it/nanochat-german-tokenizer/resolve/main/tokenizer.pkl?download=true
+
+# -----------------------------------------------------------------------------
+# Download pretraining data
+python -m nanochat.dataset -n 240
+
+# -----------------------------------------------------------------------------
+# Base model (pretraining)
+## pretrain the d20 model
+torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=20 --run=$WANDB_RUN
