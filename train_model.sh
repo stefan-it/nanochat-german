fab01a3fe50f66d12efdde43debd46d7833f5aff
Stefan Schweter <stefan@schweter.it>
2025-10-22 18:58:07 +0200

    feat: sync uv stuff with upstream

--- train_model.sh
+++ train_model.sh
@@ -22,13 +22,12 @@ command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 # create a .venv local virtual environment (if it doesn't exist)
 [ -d ".venv" ] || uv venv
 # install the repo dependencies
-uv sync
+uv sync --extra gpu
+# activate venv so that `python` uses the project's venv instead of system python
+source .venv/bin/activate
 
 # additionally install trackio
 uv pip install trackio
-
-# activate venv so that `python` uses the project's venv instead of system python
-source .venv/bin/activate
 
 # -----------------------------------------------------------------------------
 # trackio setup
