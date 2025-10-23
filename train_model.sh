4cd813ed0d53d40b114580c45d5cfe97aa31a8e0
Stefan Schweter <stefan@schweter.it>
2025-10-23 15:07:01 +0200

    feat: adjust training script (include tokenizer training)

--- train_model.sh
+++ train_model.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# This script is the "Best German ChatGPT clone that $100 can buy",
+# This script is the "Best ChatGPT clone that $100 can buy",
 # It is designed to run in ~4 hours on 8XH100 node at $3/GPU/hour.
 
-# 1) Example launch (simplest):
-# bash train_model.sh
-# 2) Example launch in a tmux session (because the run takes ~4 hours):
 # WANDB_RUN=nanochat-german tmux new-session -s nanochat-german -d "bash train_model.sh" \; pipe-pane -o "cat >> nanochat-german.log"
 
 # Default intermediate artifacts directory is in ~/.cache/nanochat
@@ -15,7 +12,6 @@ mkdir -p $NANOCHAT_BASE_DIR
 
 # -----------------------------------------------------------------------------
 # Python venv setup with uv
-export PATH=$HOME/.local/bin:$PATH
 
 # install uv (if not already installed)
 command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
@@ -25,19 +21,25 @@ command -v uv &> /dev/null || curl -LsSf https://astral.sh/uv/install.sh | sh
 uv sync --extra gpu
 # activate venv so that `python` uses the project's venv instead of system python
 source .venv/bin/activate
-
-# additionally install trackio
 uv pip install trackio
 
 # -----------------------------------------------------------------------------
-# trackio setup
-# If you wish to use trackio for logging (it's nice!, recommended).
-# Set the WANDB_RUN environment variable when running this script, e.g.:
+# wandb setup
+# If you wish to use wandb for logging (it's nice!, recommended).
+# 1) Make sure to first log in to wandb, e.g. run:
+#    `wandb login`
+# 2) Set the WANDB_RUN environment variable when running this script, e.g.:
 #    `WANDB_RUN=d26 bash speedrun.sh`
 if [ -z "$WANDB_RUN" ]; then
     # by default use "dummy" : it's handled as a special case, skips logging to wandb
     WANDB_RUN=dummy
 fi
+
+# -----------------------------------------------------------------------------
+# During the course of the run, we will be writing markdown reports to the report/
+# directory in the base dir. This command clears it out and writes a header section
+# with a bunch of system info and a timestamp that marks the start of the run.
+python -m nanochat.report reset
 
 # -----------------------------------------------------------------------------
 # Tokenizer
@@ -49,18 +51,20 @@ source "$HOME/.cargo/env"
 # Build the rustbpe Tokenizer
 uv run maturin develop --release --manifest-path rustbpe/Cargo.toml
 
-# -----------------------------------------------------------------------------
-# During the course of the run, we will be writing markdown reports to the report/
-# directory in the base dir. This command clears it out and writes a header section
-# with a bunch of system info and a timestamp that marks the start of the run.
-python -m nanochat.report reset
-
-# -----------------------------------------------------------------------------
-# Download already trained tokenizer
-mkdir -p $NANOCHAT_BASE_DIR/tokenizer
-
-wget -LO $NANOCHAT_BASE_DIR/tokenizer/token_bytes.pt https://huggingface.co/stefan-it/nanochat-german-tokenizer/resolve/main/token_bytes.pt?download=true
-wget -LO $NANOCHAT_BASE_DIR/tokenizer/tokenizer.pkl https://huggingface.co/stefan-it/nanochat-german-tokenizer/resolve/main/tokenizer.pkl?download=true
+# Download the first ~2B characters of pretraining dataset
+# look at dev/repackage_data_reference.py for details on how this data was prepared
+# each data shard is ~250M chars
+# so we download 2e9 / 250e6 = 10 data shards at this point
+# each shard is ~100MB of text (compressed), so this is about ~800MB of data on disk
+python -m nanochat.dataset -n 8
+# Immediately also kick off downloading more shards in the background while tokenizer trains
+# See comment below for why 300 is the right number here
+python -m nanochat.dataset -n 240 &
+DATASET_DOWNLOAD_PID=$!
+# train the tokenizer with vocab size 2**16 = 65536 on ~2B characters of data
+python -m scripts.tok_train --max_chars=2000000000
+# evaluate the tokenizer (report compression ratio etc.)
+python -m scripts.tok_eval
 
 # -----------------------------------------------------------------------------
 # Download evaluation data
@@ -87,13 +91,22 @@ wget -LO $NANOCHAT_EVAL_DATA_DIR/safety/enterprise_pii_classification.jsonl  htt
 wget -LO $NANOCHAT_EVAL_DATA_DIR/world_knowledge/mmlu.jsonl  https://huggingface.co/datasets/stefan-it/nanochat-german-eval-data/resolve/main/world_knowledge/mmlu.jsonl?download=true
 
 # -----------------------------------------------------------------------------
-# Download pretraining data
-python -m nanochat.dataset -n 240
+# Base model (pretraining)
 
-# -----------------------------------------------------------------------------
-# Base model (pretraining + evaluation, d20 model)
+# The d20 model is 561M parameters.
+# Chinchilla says #tokens = 20X #params, so we need 561e6 * 20 = 11.2B tokens.
+# Assume our tokenizer is 4.8 chars/token, this is 11.2B * 4.8 ~= 54B chars.
+# At 250M chars/shard, this is 54B / 250M ~= 216 shards needed for pretraining.
+# Round up to 240 for safety. At ~100MB/shard, this downloads ~24GB of data to disk.
+# (The total number of shards available in the entire dataset is 1822.)
+echo "Waiting for dataset download to complete..."
+wait $DATASET_DOWNLOAD_PID
+
+# pretrain the d20 model
 torchrun --standalone --nproc_per_node=8 -m scripts.base_train -- --depth=20 --run=$WANDB_RUN
+# evaluate the model on a larger chunk of train/val data and draw some samples
 torchrun --standalone --nproc_per_node=8 -m scripts.base_loss
+# evaluate the model on CORE tasks
 torchrun --standalone --nproc_per_node=8 -m scripts.base_eval
 
 # -----------------------------------------------------------------------------
