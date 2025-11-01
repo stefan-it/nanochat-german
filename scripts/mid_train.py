2d622e44cd917cb7f38a31ab5a0ee25309a7a9c2
Stefan Schweter <stefan@schweter.it>
2025-11-02 00:55:55 +0100

    fix: minor formatting tweak

--- scripts/mid_train.py
+++ scripts/mid_train.py
@@ -119,7 +119,7 @@ train_dataset = TaskMixture([
     GermanSimpleSpelling(size=80_000, split="train"),
     Wmt19(split="train", direction="en-de", size=400_000),
     Wmt19(split="train", direction="de-en", size=400_000),
-]) # total: 50_469 + 706 + 706 + 15_015 + 13_976 + 59_022 + 9_829 + 238_658 + 6_101 + 51_095 + 200_000 + 80_000 + 400_000 + 400_000 = 1_525_577
+]) # total: 50_469 + 706 + 706 + 15_015 + 13_976 + 59_022 + 9_829 + 238_658 + 6_099 + 51_095 + 200_000 + 80_000 + 400_000 + 400_000 = 1_525_577
 
 val_dataset = TaskMixture([
     GermanGuanako(split="test"),
